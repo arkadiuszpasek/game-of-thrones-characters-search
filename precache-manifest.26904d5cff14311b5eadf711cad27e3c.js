f13f92fe2150524ed1cdb22a92fe85e1aa833266
Arkadiusz Pasek <thevik444@gmail.com>
2020-03-08 10:26:08 +0100

    Updates

---
+++ precache-manifest.26904d5cff14311b5eadf711cad27e3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89886a439e0412693d0d4568e69596a",
+    "url": "/game-of-thrones/index.html"
+  },
+  {
+    "revision": "8302b79c20c143312a12",
+    "url": "/game-of-thrones/static/css/main.197080d8.chunk.css"
+  },
+  {
+    "revision": "b9f505cf1d0676e1d00b",
+    "url": "/game-of-thrones/static/js/2.b8af7389.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/game-of-thrones/static/js/2.b8af7389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8302b79c20c143312a12",
+    "url": "/game-of-thrones/static/js/main.50b154fb.chunk.js"
+  },
+  {
+    "revision": "ff2305abb70cfd8baaaa",
+    "url": "/game-of-thrones/static/js/runtime-main.5fe7dd1a.js"
+  }
+]);
